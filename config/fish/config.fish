90c0f41bbbf03fbf81c3bb3635ecaddcded94c03
Alfred MOUELLE <alfredmouelle@gmail.com>
2022-10-31 17:44:37 +0100

    updating fish conf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Support to use  the !! for run previous command 
+# Support to use  the !! for run previous command
 function sudo
     if test "$argv" = !!
         eval command sudo $history[1]
@@ -12,34 +12,41 @@ function sudo
 end
 
 # Pacman
-abbr -a -g pmc-s "pacman -Ss"
-abbr -a -g pmc-r "sudo pacman -Rc"
-abbr -a -g pmc-i "sudo pacman -S"
-abbr -a -g pmc-cc "sudo pacman -Scc"
-abbr -a -g pmc-u "sudo pacman -Sy"
-abbr -a -g pmc-su "sudo pacman -Syu"
+abbr -a -g pmcs "pacman -Ss"
+abbr -a -g pmcr "sudo pacman -Rc"
+abbr -a -g pmci "sudo pacman -S"
+abbr -a -g pmccc "sudo pacman -Scc"
+abbr -a -g pmcu "sudo pacman -Sy"
+abbr -a -g pmcsu "sudo pacman -Syu"
 
 # Yay
-abbr -a -g yy-s "yay -Ss"
-abbr -a -g yy-i "yay -S"
-abbr -a -g yy-r "yay -Rc"
-abbr -a -g yy-u "yay -Sy"
-abbr -a -g yy-su "yay -Syu"
-abbr -a -g yy-cc "yay -Sc"
+abbr -a -g yys "yay -Ss"
+abbr -a -g yyi "yay -S"
+abbr -a -g yyr "yay -Rc"
+abbr -a -g yyu "yay -Sy"
+abbr -a -g yysu "yay -Syu"
+abbr -a -g yycc "yay -Sc"
 
 # Config
 abbr -a -g polyconf "vim $HOME/.config/polybar/config"
 abbr -a -g i3conf "vim $HOME/.config/i3/config"
 abbr -a -g dunstconf "vim $HOME/.config/dunst/dunstrc"
-abbr -a -g crittyconf "vim $HOME/.config/alacritty/alacritty.yml"
 
 # Short hand
-abbr -a -g vscupdate "yay -Sy && yay -S visual-studio-code-bin"
+abbr -a -g vscup "yay -Sy && yay -S visual-studio-code-bin"
 
 # Aura Dev
 abbr -a -g mkcls "make clean"
 abbr -a -g mkup "make update PM=pnpm"
 abbr -a -g mksvr "make server PM=pnpm"
+
+# Git
+abbr -a -g gtf "git fetch"
+abbr -a -g gtpl "git pull"
+abbr -a -g gtps "git push"
+abbr -a -g gts "git stash"
+abbr -a -g gtsa "git stash apply"
+abbr -a -g gtsd "git stash drop"
 
 abbr -a -g fishsrc "source ~/.config/fish/config.fish"
 
